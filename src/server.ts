220df8ae4006bcfd957dbb680a8e45b61a17ab0c
Pedro <pedroxad@gmail.com>
2024-04-09 12:08:57 -0300

    create post events route

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
-import fastify from "fastify"
+import fastify from "fastify";
+import {z} from "zod";
+import {PrismaClient} from '@prisma/client';
+
+const app = fastify()
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 // Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, ...
 
@@ -10,13 +18,31 @@ import fastify from "fastify"
 
 // Semânticas = Significado
 
+// Drive nativo / Query Builders / ORMs
 
-const app = fastify()
+// Object Relational Mapping (Hibernate / Doctrine / ActiveRecord)
 
-app.get('/', () => {
-    return 'Hello APP'
+    app.post('/events', async (request, replay) =>{
+        const createEventSchema = z.object({
+            title: z.string().min(4),
+            details: z.string().nullable(),
+            maximumAttendees: z.number().int().positive().nullable(),
+    })
+
+    const data = createEventSchema.parse(request.body)
+
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toISOString(),
+        },
+    })
+
+    return replay.status(201).send({ eventId: event.id });
 })
 
 app.listen({ port: 3333 }).then(() => {
-    console.log('HTTP server running!')
+    console.log(`Server is running`)
 })
