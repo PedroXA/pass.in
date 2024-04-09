c362b0e8417a9c198c0451725f5fa8efe149054e
Pedro <pedroxad@gmail.com>
2024-04-09 10:03:30 -0300

    Adição de comentários

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import fastify from "fastify"
 
 // Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, ...
-// Corpo da requisição
-// Parâmetros de busca
-// Parâmetros de rota
-// Cabeçalhos
+
+// Corpo da requisição (Request Body)
+// Parâmetros de busca (Search Params / Query Params) `http://localhost:3333/users?name=Pedro`
+// Parâmetros de rota  (Route Params) -> Identificação de recursos `DELETE http://localhost:3333/users/5`
+// Cabeçalhos  (Headers) -> Contexto 
+
+
+// Semânticas = Significado
+
 
 const app = fastify()
 
